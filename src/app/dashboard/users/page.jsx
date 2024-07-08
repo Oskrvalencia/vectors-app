f8e4ee9f2c97f6286d3d6a302cc00333a9f8b9be
Oskrvalencia <ovalencia08@misena.edu.co>
2024-07-08 14:59:09 -0500

    fix: bug deploy

--- src/app/dashboard/users/page.jsx
+++ src/app/dashboard/users/page.jsx
@@ -5,7 +5,7 @@ import React, { useContext } from "react";
 import MapContext from "@/context/MapContext";
 
 export default function Users() {
-  const { option, setOption, session } = useContext(MapContext);
+  const { session } = useContext(MapContext);
 
   return (
     <div class="bg-slate-900 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl m-10 ">
@@ -13,13 +13,13 @@ export default function Users() {
         <h3 class=" text-white mr-8 text-base font-medium tracking-tight col-4">
           User:
         </h3>
-        <p class="text-slate-400 text-sm col-8">{session.user.name}</p>
+        <p class="text-slate-400 text-sm col-8">{session?.user?.name}</p>
       </div>
       <div class="bg-slate-800 rounded-lg px-6 py-4 ring-1 ring-slate-900/5 shadow-xl flex mt-5">
         <h3 class=" text-white mr-8 text-base font-medium tracking-tight col-4">
           Email:
         </h3>
-        <p class="text-slate-400 text-sm col-8">{session.user.email}</p>
+        <p class="text-slate-400 text-sm col-8">{session?.user?.email}</p>
       </div>
     </div>
   );
